5cb68efe37406ae8c0b49864aa8ea1ce3dd00a6e
Wai Chun Leung <wc.leung@hotmail.com>
2023-10-25 22:40:15 +0200

    feat: Added better exception placements. Can now download images when src doesn't include domain

--- scraper.py
+++ scraper.py
@@ -3,6 +3,8 @@ import os
 import time
 import re
 
+from urllib.parse import urlparse
+
 import requests  
 from PIL import Image
 from bs4 import BeautifulSoup  
@@ -21,22 +23,36 @@ def main(argv):
 
     if url != '':
         try:
+            domain = urlparse(url).netloc
+            http_version = url.split('/')[0]
+
             htmldata = getdata(url)
             soup = BeautifulSoup(htmldata, 'html.parser')   
 
             cleanString = re.sub(r'\W+', '', url)
-
-            if (not os.path.exists(cleanString)):
-                os.makedirs(cleanString)
-
-            for item in soup.find_all('img'): 
-                if (item['src'].split('.')[-1] == 'jpg' or item['src'].split('.')[-1] == 'png' or item['src'].split('.')[-1] == 'webp'):
-                    print('Downloading', item['src'])
-
-                    img = Image.open(requests.get(item['src'], stream = True).raw)
-                    img.save(f'{cleanString}/{item['src'].split('/')[-1]}')
         except Exception as e:
             print(e)
+            return 
+
+        if not os.path.exists(cleanString):
+            os.makedirs(cleanString)
+
+        for item in soup.find_all('img'):   
+            try:
+                file_ext = item.get('src', '').split('.')[-1]
+
+                if file_ext == 'jpg' or file_ext == 'png' or file_ext == 'webp':
+                    print('Downloading', item['src'])
+
+                    if 'http' not in item['src']:
+                        img = Image.open(requests.get(f'{http_version}//{domain}{item['src']}', stream = True).raw)
+                    else:
+                        img = Image.open(requests.get(item['src'], stream = True).raw)
+
+                    img.save(f'{cleanString}/{item['src'].split('/')[-1]}')
+            except Exception as e:
+                print(e)
+                continue
 
 if __name__ == '__main__':
    main(sys.argv[1:])
